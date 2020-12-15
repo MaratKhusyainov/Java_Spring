331dcc858c27ecb3b13315005365940654b050be
MaratKhusyainov <khusyainov.mar@yandex.ru>
2020-12-15 22:23:30 +0300

    Домашнее задание № 3

---
+++ src/main/java/com/spring/Hometask3/Product.java
@@ -0,0 +1,16 @@
+package com.spring.Hometask3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+}
